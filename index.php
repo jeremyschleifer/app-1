59bc7daf19cf0ac7e361267986f17dec76adf196
jeremyschleifer <jeremy.schleifer@gmail.com>
2016-07-21 16:05:05 -0400

    set up bootstrap, dependencies

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+		<link rel="stylesheet" type="text/css" href="css/custom.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<p>text test</p>
+				</div>
+				<div class="col-md-4">
+					<p>testy test</p>
+				</div>
+			</div>
+			
+		</div>
+
+
+
+
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/npm.js"></script>
+	<script src="js/custom.js"></script>
+	</body>
+</html>
